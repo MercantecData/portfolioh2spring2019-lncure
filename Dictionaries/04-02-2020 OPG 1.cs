394dbe24405ff9ac23093ab21ce6b3869ae3923e
Victor Phasen <vnlo.skp@edu.mercantec.dk>
2020-02-04 09:07:25 +0100

    I finished the second assignment
    this one involved floats and bools, pretty epic.

---
+++ Dictionaries/04-02-2020 OPG 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        // - Lav et dictionary der kan gemme tal, og bruge strings som key. Gem 10 forskellige værdier heri.
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> testionary = new Dictionary<string, int>();
+
+            testionary["a"] = 1;
+            testionary["b"] = 2;
+            testionary["c"] = 3;
+            testionary["d"] = 4;
+            testionary["e"] = 5;
+            testionary["f"] = 6;
+            testionary["g"] = 7;
+            testionary["h"] = 8;
+            testionary["i"] = 9;
+            testionary["j"] = 10;
+        }
+    }
+}
---
+++ Dictionaries/04-02-2020 OPG 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        // - Lav et dictionary der kan gemme booleans og bruge float som key. Gem 10 forskellige værdier heri.
+        static void Main(string[] args)
+        {
+            Dictionary<float, bool> testionary = new Dictionary<float, bool>();
+
+            testionary[1.1f] = true;
+            testionary[1.2f] = false;
+            testionary[1.3f] = true;
+            testionary[1.4f] = false;
+            testionary[1.5f] = true;
+            testionary[1.6f] = false;
+            testionary[1.7f] = true;
+            testionary[1.8f] = false;
+            testionary[1.9f] = true;
+            testionary[1.10f] = false;
+        }
+    }
+}
