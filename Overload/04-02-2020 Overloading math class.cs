7558b99bae5caeaab2e643055942b8d8987233c3
Victor Phasen <vnlo.skp@edu.mercantec.dk>
2020-02-04 12:23:06 +0100

    I finished the First assignment in Overloads
    Confusing at first, but i think i under stand the gist of it.

---
+++ Overload/04-02-2020 Overloading math class.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Overloading_mathclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(math.Add("hjælp ","mig"));
+            Console.ReadKey();
+
+        }
+
+        public class math
+        {
+            //------------------------------------------------------
+            public static int Add (int a, int b)
+            {
+                return a + b; 
+            }
+            public static int Subtract(int a, int b)
+            {
+                return a - b;
+            }
+            public static int Divide(int a, int b)
+            {
+                return a / b;
+            }
+            public static int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+
+            //-------------------------------------------------------
+
+            public static float Add(float a, float b)
+            {
+                return a + b;
+            }
+            public static float Subtract(float a, float b)
+            {
+                return a + b;
+            }
+            public static float Divide(float a, float b)
+            {
+                return a + b;
+            }
+            public static float Multiply(float a, float b)
+            {
+                return a + b;
+            }
+
+            //--------------------------------------------------------
+
+            public static string Add(string a, string b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
