4fbe1dd222c73d1223e02116776e0f81a1534e71
Victor Phasen <vnlo.skp@edu.mercantec.dk>
2020-02-05 09:27:50 +0100

    I've finished the lambda assignments
    These were the spiciest in town.

---
+++ Lambda Expressions/05-02-2020 Lambda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lambda_Expression
+{
+    class Program
+    {
+        // - Lav en Lambda Expression der tager imod en int og returnerer tallet fordoblet. Gem i en passende delegate, og kør.
+        public delegate int Opg1Delegate(int number);
+
+        // - Lav en Lambda Expression der tager imod 3 floats og returnerer summen. Gem i en passende delegate, og kør.
+        public delegate float Opg2Delegate(float a, float b, float c);
+
+        // - Lav en Lambda Expression der ikke tager imod noget og returnerer "Hello World". Gem i en passende delegate og kør.
+        public delegate string Opg3Delegate();
+        //---------------------------------------------------------------------------------------------------------------------
+
+        static void Main(string[] args)
+        {
+            Opg1Delegate del1 = (x) => x + x;
+
+            Console.WriteLine(del1(12)); //12 er basically det vil sætter ind for at overtage x's plads.
+
+            //----------------------------------------------------------------------------------------
+
+            Opg2Delegate del2 = (a, b, c) => a + b + c;
+
+            Console.WriteLine(del2(12.5f, 3.4f, 2.5f));
+
+            //----------------------------------------------------------------------------------------
+
+            Opg3Delegate del3 = () => "Hello World";
+
+            Console.WriteLine((del3()));
+
+            //----------------------------------------------------------------------------------------
+        }
+    }
+}
