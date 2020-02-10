d52faaa5582bf105fddfc0ceb505984a73a37f34
Victor Phasen <vnlo.skp@edu.mercantec.dk>
2020-02-10 12:42:45 +0100

    Made some changes to abstract classes, and made the number guesser
    Noticed that i misunderstood the abstract assignment, so i'm currently making it again. Hoping i can finish it today.

---
+++ Abstract and Interface/Abstract and Interface/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abstract_and_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // - Lav en abstrakt klasse med minimum én abstrakt metode i. Instantier 3 forskellige subklasser af denne.
+        public abstract class test
+        {
+            public abstract string methodName();
+
+        }
+
+        public class subclass1 : test
+        {
+            public override string methodName()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class subclass2 : test
+        {
+            public override string methodName()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class subclass3 : test
+        {
+            public override string methodName()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // - Lav et interface med 3 metoder i. Implementer dette interface i 2 forskellige classes.
+        interface INewInterface
+        {
+            void Testing1();
+            void Testing2();
+            void Testing3();
+        }
+
+        public class InterfaceTest1 : INewInterface
+        {
+
+            public void Testing1()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Testing2()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Testing3()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class InterfaceTest2 : INewInterface
+        {
+
+            public void Testing1()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Testing2()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Testing3()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // - Lav en liste på minimum 10 personer med Navn, Alder og Køn. Brug IComparable til at sortere efter Alder.
+        public class Person : IComparable
+        {
+            string name;
+            int age;
+            bool gender; //True = male, False = female.
+
+            public Person(string name, int age, bool gender)
+            {
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+            }
+
+            public int CompareTo(object obj)
+            {
+                Person persons = obj as Person;
+                if (persons != null)
+                {
+                    return this.age.CompareTo(persons.age);
+                }
+                else
+                {
+                    throw new NullReferenceException("GET THIS BETA SHIT OFF MEE!! AAAAAAAH!");
+                }
+            }
+        }
+
+        //- Lav en liste på minimum 10 personer med Navn, Alder og Køn. Brug IComparable til at sortere efter Alder.
+        public class machmudsLair
+        {
+            List<Person> Persons = new List<Person>();
+            public machmudsLair()
+            {
+                Persons.Add(new Person("Machmud", 69, true));
+                Persons.Add(new Person("Logan", 18, true));
+                Persons.Add(new Person("Mads", 13, false));
+                Persons.Add(new Person("Mark", 69, true));
+                Persons.Add(new Person("Nikolaj", 34, true));
+                Persons.Add(new Person("yashar", 40, true));
+                Persons.Add(new Person("Pinar", 17, true));
+                Persons.Add(new Person("Spankbank", 88, false));
+                Persons.Add(new Person("johanne", 56, false));
+                Persons.Add(new Person("jay", 89, true));
+            }
+        }
+        // - Ændre dit program til at sortere alfabetisk. Hvis 2 personer hedder det samme, sorter efter alder. Hvis de så har samme, sorter kvinder før mænd. Brug stadig IComparable.
+    }
+}
---
+++ Multithreading med Tasks/Multithreading med Tasks/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Multithreading_med_Tasks
+{
+    class Program
+    {
+        static bool timeout = false;
+        static void Main(string[] args)
+        {
+            //------------------------------------------------------------------------
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("||A random number has been chosen!||");
+            Console.WriteLine("||You must guess what it could be!||");
+            Console.WriteLine("||before the timer hits zero!!!!!!||");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Press 'Enter' When you're ready!");
+            Console.ReadLine();
+
+            //------------------------------------------------------------------------
+
+            Task t = DoMyTask();
+            //------------------------------------------------------------------------
+            Random random = new Random();
+            int returnValue = random.Next(0, 10);
+            //------------------------------------------------------------------------
+
+            int userGuess = 0;
+
+            while (userGuess != returnValue)
+            {
+                try
+                {
+                    userGuess = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                }
+
+                if (timeout)
+                {
+                    Console.WriteLine("Du er løbet tør for tid din fucking idiot");
+                    return;
+                }
+
+                if (userGuess == returnValue)
+                {
+                    Console.WriteLine("You're gay lmao, btw you won");
+                    return;
+                }
+                Console.WriteLine("Wrong! try again!");
+            }
+        }
+
+        static async Task DoMyTask()
+        {
+            int numberCount = 10;
+
+            while (numberCount != 0)
+            {
+                Console.WriteLine("You have " + numberCount + " seconds left to answer");
+                numberCount--;
+                await Task.Delay(1000);
+            }
+            Console.WriteLine("How many seconds... in eternity?");
+            timeout = true;
+        }
+    }
+}
